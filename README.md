c7f24e3ada3b81d84a60737caada8a19b5325978
Tyler Mauthe <tmauthe@Tylers-MacBook-Pro.local>
2015-11-25 22:05:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Strap
+Hatstrap is a script to bootstrap a minimal OS X development system, forked from [Strap](https://github.com/mikemcquaid/strap)
+## Features
+- Enables the OS X screensaver password immediately (for better security)
+- Adds a `Found this computer?` message to the login screen (for machine recovery)
+- Installs the Xcode Command Line Tools (for compilers and Unix tools)
+- Agree to the Xcode license (for using compilers without prompts)
+- Installs [Homebrew](http://brew.sh) (for installing command-line software)
+- Installs [Homebrew Versions](https://github.com/Homebrew/homebrew-versions) (for installing older versions of command-line software)
+- Installs [Homebrew Bundle](https://github.com/Homebrew/homebrew-bundle) (for `bundler`-like `Brewfile` support)
+- Installs [Homebrew Services](https://github.com/Homebrew/homebrew-services) (for managing Homebrew-installed services)
+- Installs [Homebrew Cask](https://github.com/caskroom/homebrew-cask) (for installing graphical software)
+- Installs the latest OS X software updates (for better security)
+
+## License
+Hatstrap is forked from Strap and trimmed to fit.
+
+Strap is licensed under the [MIT License](http://en.wikipedia.org/wiki/MIT_License).
+The full license text is available in [LICENSE.txt](https://github.com/mikemcquaid/strap/blob/master/LICENSE.txt).
---
+++ bin/hatstrap.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+#/ Usage: bin/hatstrap.sh [--debug]
+#/ Install development dependencies on Mac OS X.
+set -e
+
+# Keep sudo timestamp updated while Strap is running.
+if [ "$1" = "--sudo-wait" ]; then
+  while true; do
+    sudo -v
+    sleep 1
+  done
+  exit 0
+fi
+
+[ "$1" = "--debug" ] && HATSTRAP_DEBUG="1"
+HATSTRAP_SUCCESS=""
+
+cleanup() {
+  set +e
+  if [ -n "$HATSTRAP_SUDO_WAIT_PID" ]; then
+    sudo kill "$HATSTRAP_SUDO_WAIT_PID"
+  fi
+  sudo -k
+  rm -f "$CLT_PLACEHOLDER" "$HATSTRAP_BREWFILE"
+  if [ -z "$HATSTRAP_SUCCESS" ]; then
+    if [ -n "$HATSTRAP_STEP" ]; then
+      echo "!!! $HATSTRAP_STEP FAILED" >&2
+    else
+      echo "!!! FAILED" >&2
+    fi
+    if [ -z "$HATSTRAP_DEBUG" ]; then
+      echo "!!! Run '$0 --debug' for debugging output." >&2
+      echo "!!! If you're stuck: file an issue with debugging output at:" >&2
+      echo "!!!   $HATSTRAP_ISSUES_URL" >&2
+    fi
+  fi
+}
+
+trap "cleanup" EXIT
+
+if [ -n "$HATSTRAP_DEBUG" ]; then
+  set -x
+else
+  HATSTRAP_QUIET_FLAG="-q"
+  Q="$HATSTRAP_QUIET_FLAG"
+fi
+
+STDIN_FILE_DESCRIPTOR="0"
+[ -t "$STDIN_FILE_DESCRIPTOR" ] && HATSTRAP_INTERACTIVE="1"
+
+HATSTRAP_NAME=
+HATSTRAP_EMAIL=
+HATSTRAP_ISSUES_URL="https://github.com/tylermauthe/hatstrap/issues/new"
+
+HATSTRAP_FULL_PATH="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
+
+abort() { HATSTRAP_STEP="";   echo "!!! $@" >&2; exit 1; }
+log()   { HATSTRAP_STEP="$@"; echo "--> $@"; }
+logn()  { HATSTRAP_STEP="$@"; printf -- "--> $@ "; }
+logk()  { HATSTRAP_STEP="";   echo "OK"; }
+
+sw_vers -productVersion | grep $Q -E "^10.(9|10|11)" || {
+  abort "Run Strap on Mac OS X 10.9/10/11."
+}
+
+[ "$USER" = "root" ] && abort "Run Strap as yourself, not root."
+groups | grep $Q admin || abort "Add $USER to the admin group."
+
+# Initialise sudo now to save prompting later.
+log "Enter your password (for sudo access):"
+sudo -k
+sudo /usr/bin/true
+[ -f "$HATSTRAP_FULL_PATH" ]
+sudo bash "$HATSTRAP_FULL_PATH" --sudo-wait &
+HATSTRAP_SUDO_WAIT_PID="$!"
+ps -p "$HATSTRAP_SUDO_WAIT_PID" 2>&1 >/dev/null
+logk
+
+# Set some basic security settings.
+logn "Configuring security settings:"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+if [ -n "$HATSTRAP_NAME" ] && [ -n "$HATSTRAP_EMAIL" ]; then
+  sudo defaults write /Library/Preferences/com.apple.loginwindow \
+    LoginwindowText \
+    "Found this computer? Please contact $HATSTRAP_NAME at $HATSTRAP_EMAIL."
+fi
+logk
+
+# Install the Xcode Command Line Tools if Xcode isn't installed.
+DEVELOPER_DIR=$("xcode-select" -print-path 2>/dev/null || true)
+[ -z "$DEVELOPER_DIR" ] || ! [ -f "$DEVELOPER_DIR/usr/bin/git" ] && {
+  log "Installing the Xcode Command Line Tools:"
+  CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+  sudo touch "$CLT_PLACEHOLDER"
+  CLT_PACKAGE=$(softwareupdate -l | \
+                grep -B 1 -E "Command Line (Developer|Tools)" | \
+                awk -F"*" '/^ +\*/ {print $2}' | sed 's/^ *//' | head -n1)
+  sudo softwareupdate -i "$CLT_PACKAGE"
+  sudo rm -f "$CLT_PLACEHOLDER"
+  logk
+}
+
+# Check if the Xcode license is agreed to and agree if not.
+xcode_license() {
+  if /usr/bin/xcrun clang 2>&1 | grep $Q license; then
+    if [ -n "$HATSTRAP_INTERACTIVE" ]; then
+      logn "Asking for Xcode license confirmation:"
+      sudo xcodebuild -license
+      logk
+    else
+      abort 'Run `sudo xcodebuild -license` to agree to the Xcode license.'
+    fi
+  fi
+}
+xcode_license
+
+# Setup Git
+logn "Configuring Git:"
+if [ -n "$HATSTRAP_NAME" ] && ! git config user.name >/dev/null; then
+  git config --global user.name "$HATSTRAP_NAME"
+fi
+
+if [ -n "$HATSTRAP_EMAIL" ] && ! git config user.email >/dev/null; then
+  git config --global user.email "$HATSTRAP_EMAIL"
+fi
+
+# Setup Homebrew directories and permissions.
+logn "Installing Homebrew:"
+HOMEBREW_PREFIX="/usr/local"
+HOMEBREW_CACHE="/Library/Caches/Homebrew"
+for dir in "$HOMEBREW_PREFIX" "$HOMEBREW_CACHE"; do
+  [ -d "$dir" ] || sudo mkdir -p "$dir"
+  sudo chown -R $USER:admin "$dir"
+done
+
+# Download Homebrew.
+export GIT_DIR="$HOMEBREW_PREFIX/.git" GIT_WORK_TREE="$HOMEBREW_PREFIX"
+git init $Q
+git config remote.origin.url "https://github.com/Homebrew/homebrew"
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git rev-parse --verify --quiet origin/master >/dev/null || {
+  git fetch $Q origin master:refs/remotes/origin/master --no-tags --depth=1
+  git reset $Q --hard origin/master
+}
+sudo chmod g+rwx "$HOMEBREW_PREFIX"/* "$HOMEBREW_PREFIX"/.??*
+unset GIT_DIR GIT_WORK_TREE
+logk
+
+export PATH="$HOMEBREW_PREFIX/bin:$PATH"
+log "Updating Homebrew:"
+brew update
+logk
+
+# Install Homebrew Bundle, Cask, Services and Versions tap.
+log "Installing Homebrew taps and extensions:"
+brew tap | grep -i $Q Homebrew/bundle || brew tap Homebrew/bundle
+HATSTRAP_BREWFILE="/tmp/Brewfile.strap"
+cat > "$HATSTRAP_BREWFILE" <<EOF
+tap 'caskroom/cask'
+tap 'homebrew/services'
+tap 'homebrew/versions'
+brew 'caskroom/cask/brew-cask'
+EOF
+brew bundle --file="$HATSTRAP_BREWFILE"
+rm -f "$HATSTRAP_BREWFILE"
+logk
+
+# Check and install any remaining software updates.
+logn "Checking for software updates:"
+if softwareupdate -l 2>&1 | grep $Q "No new software available."; then
+  logk
+else
+  echo
+  log "Installing software updates:"
+  if [ -z "$HATSTRAP_CI" ]; then
+    sudo softwareupdate --install --all
+    xcode_license
+  else
+    echo "Skipping software updates for CI"
+  fi
+  logk
+fi
+
+HATSTRAP_SUCCESS="1"
+log 'Finished! Install additional software with `brew install` and `brew cask install`.'
