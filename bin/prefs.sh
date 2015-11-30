65980ae52ccb209f809af2fe0ee9ed52f809aae0
Tyler Mauthe <tmauthe@hatsize.com>
2015-11-29 19:05:21 -0700

    Remove some Google Chrome prefs

--- bin/prefs.sh
+++ bin/prefs.sh
@@ -650,14 +650,6 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 defaults write com.google.Chrome ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
 defaults write com.google.Chrome.canary ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
 
-# Use the system-native print preview dialog
-defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome.canary DisablePrintPreview -bool true
-
-# Expand the print dialog by default
-defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
-defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
-
 ###############################################################################
 # Twitter.app                                                                 #
 ###############################################################################
