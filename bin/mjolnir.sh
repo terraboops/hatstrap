c028b85e4a09c735fac98bb441e94493a46cdc7b
Tyler Mauthe <tmauthe@hatsize.com>
2016-02-15 14:09:34 -0700

    Fix Mjolnir modules

--- bin/mjolnir.sh
+++ bin/mjolnir.sh
@@ -4,3 +4,7 @@
 echo 'rocks_servers = { "http://rocks.moonscript.org" }' >> /usr/local/etc/luarocks52/config-5.2.lua
 luarocks install mjolnir.hotkey
 luarocks install mjolnir.application
+luarocks install mjolnir.fnutils
+luarocks install mjolnir.geometry
+luarocks install mjolnir.screen
+luarocks install mjolnir.keycodes
